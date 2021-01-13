637c6bcb4b90345ccb5b3126849f79fb4f74aff1
Tanveer81 <tanveer.hannan@campus.lmu.de>
2021-01-13 21:57:22 +0100

    steganogan image generation script added

---
+++ steganogan_image_generation.py
@@ -0,0 +1,21 @@
+import os
+import steganogan
+from steganogan import SteganoGAN
+import random
+import string
+
+def get_random_alphanumeric_string(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
+    return result_str
+
+steganogan = SteganoGAN.load(architecture='dense')
+
+i = 0
+for filename in sorted(os.listdir('alaska2-image-steganalysis/Cover/')):
+    if i==20:
+        break
+    i = i + 1
+    steganogan.encode(f'alaska2-image-steganalysis/Cover/{filename}',
+                      f"alaska2-image-steganalysis/SteganoGAN/{filename.replace('jpg', 'png')}",
+                      get_random_alphanumeric_string(random.randint(10, 50)))
