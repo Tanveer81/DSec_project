e0c540340cea07c4186ccfaa3c93111c7a670a0e
Tanveer81 <tanveer.hannan@campus.lmu.de>
2021-01-14 09:06:15 +0100

    folder structure fix

---
+++ scripts/evaluation.py
@@ -0,0 +1,32 @@
+from glob import glob
+from sklearn.model_selection import GroupKFold
+import cv2
+from skimage import io
+import torch
+from torch import nn
+import os
+from datetime import datetime
+import time
+import random
+import cv2
+import pandas as pd
+import numpy as np
+import albumentations as A
+import matplotlib.pyplot as plt
+from albumentations.pytorch.transforms import ToTensorV2
+from torch.utils.data import Dataset,DataLoader
+from torch.utils.data.sampler import SequentialSampler, RandomSampler
+import sklearn
+
+SEED = 512
+
+def seed_everything(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+seed_everything(SEED)
